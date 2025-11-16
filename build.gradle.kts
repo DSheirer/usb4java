4bd956e3cbfe3124a83515d0909e821fb928f86e
denny <dennis.sheirer@gmail.com>
2025-11-16 15:31:28 -0500

    build script updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,16 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
+    implementation("org.junit.jupiter:junit-jupiter-api:6.0.1")
+    implementation("org.junit.jupiter:junit-jupiter-engine:6.0.1")
+    implementation("org.junit.jupiter:junit-jupiter-params:6.0.1")
+    implementation("org.junit.vintage:junit-vintage-engine:6.0.1")
+    implementation("org.apache.commons:commons-lang3:3.19.0")
+    implementation("io.github.dsheirer:usb4java-native-libraries:1.3.1") //OSX & Windows aarch64 native libs
+    implementation("org.usb4java:libusb4java:1.3.0")
+    implementation("org.usb4java:usb4java-javax:1.3.0")}
 
-tasks.test {
-    useJUnitPlatform()
+tasks.withType<Test>() {
+    //Disable USB testing
+    systemProperty("USB_TESTS", "false")
 }
